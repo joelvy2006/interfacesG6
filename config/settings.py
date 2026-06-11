096cc257dc3f573c68967c0c8f471bda0d494c3f
veganayeli925-wq <veganayeli925@gmail.com>
2026-06-11 14:34:28 -0500

    cambio

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'plantacion',             
-        'USER': 'postgres',
-        'PASSWORD': 'joelvargas2006',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -122,6 +118,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard/'
 
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
